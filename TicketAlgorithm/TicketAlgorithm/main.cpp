434d4adf5acf8fce21ed010a0e66d442556a2047
JackFennell2195 <JackFennell2195@users.noreply.github.com>
2020-01-30 15:25:02 +0000

    Lab8 start

---
+++ TicketAlgorithm/TicketAlgorithm/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <atomic>
+#include <thread>
+#include <mutex>
+
+using namespace std;
+
+const int n = 10;
+int number;
+int next;
+int turn[n];
+mutex Mutex;
+
+void func() 
+{	
+	
+}
+
+int main()
+{
+
+	return 0;
+}
